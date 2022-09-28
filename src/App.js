968c204fad9546ee0de5abcdf805a8ee3f459747
Meghakumarigit <kmegha2017@gmail.com>
2022-09-28 22:15:24 +0530

    formatting

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import ListEmployeeComponent from './components/ListEmployeeComponent';
-import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import logo from "./logo.svg";
+import "./App.css";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //import { BrowserRouter, Routes, Route } from "react-router-dom"
-import CreateEmployeeComponent from './components/CreateEmployeeComponent';
-
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
 
 function App() {
   return (
     <div>
-      
-        <HeaderComponent/>
-        <div className="container">
-          <Router>
-        <Switch>
-        
-        <Route path="/add-employee" component={CreateEmployeeComponent} />
-          <Route path="/" exact component={ListEmployeeComponent}/>
+      <HeaderComponent />
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route path="/add-employee" component={CreateEmployeeComponent} />
+            <Route path="/" exact component={ListEmployeeComponent} />
             {/* <Route path="/employees" component={ListEmployeeComponent} /> */}
-            
-           
-            
-        </Switch>
+          </Switch>
         </Router>
-        </div>
-        <FooterComponent/>
-      
+      </div>
+      <FooterComponent />
     </div>
   );
 }
